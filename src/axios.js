c1f77e60de46850eb06a5af3b635d142083e5927
userDymon <dimagrabovskuy@gmail.com>
2024-06-20 01:54:05 +0300

    frontend v0.2.5

--- src/axios.js
+++ src/axios.js
@@ -4,6 +4,4 @@ const instance = axios.create({
     baseURL: 'http://localhost:4444',
 });
 
-axios.get('/posts');
-
 export default instance;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,17 +7,19 @@ import Grid from '@mui/material/Grid';
 import { Post } from '../components/Post';
 import { TagsBlock } from '../components/TagsBlock';
 import { CommentsBlock } from '../components/CommentsBlock';
-import { fetchPosts } from '../redux/slices/posts';
+import { fetchPosts, fetchTags } from '../redux/slices/posts';
 
 export const Home = () => {
   const dispatch = useDispatch();
   const {posts, tags} = useSelector(state => state.posts);
 
   const isPostsLoading = posts.status === 'loading';
+  const isTagsLoading = tags.status === 'loading';
 
   React.useEffect(() => {
     dispatch(fetchPosts());
-  }, []);
+    dispatch(fetchTags());
+  }, [dispatch]); 
 
   return (
     <>
@@ -31,19 +33,18 @@ export const Home = () => {
             <Post
               id={obj._id}
               title={obj.title}
-              imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
+              imageUrl={obj.imageUrl}
               user={obj.user}
               createdAt={obj.createdAt}
               viewsCount={obj.viewsCount}
               commentsCount={3}
               tags={obj.tags}
-              isLoading={true}
               isEditable
             />
           ))}
         </Grid>
         <Grid xs={4} item>
-          <TagsBlock items={['react', 'typescript', 'notes']} isLoading={false} />
+          <TagsBlock items={tags.items} isLoading={isTagsLoading} />
           <CommentsBlock
             items={[
               {
