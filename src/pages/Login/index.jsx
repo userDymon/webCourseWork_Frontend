5517f4e9ecccc979f8051d2645de301cacfa60cd
userDymon <dimagrabovskuy@gmail.com>
2024-06-26 23:35:42 +0300

    add authorization

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,28 +1,62 @@
 import React from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
+import { useForm } from "react-hook-form";
 
 import styles from "./Login.module.scss";
+import { fetchAuth } from "../../redux/slices/auth";
 
 export const Login = () => {
+  const dispatch = useDispatch();
+  const { 
+    register, 
+    handleSubmit, 
+    setError, 
+    formState: { errors, isValid },
+  } = useForm({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+    mode: "onChange",
+  });
+
+  const onSubmit = (values) => {
+    dispatch(fetchAuth(values));
+  };
+
   return (
     <Paper classes={{ root: styles.root }}>
       <Typography classes={{ root: styles.title }} variant="h5">
         Login to your account
       </Typography>
-      <TextField
-        className={styles.field}
-        label="E-Mail"
-        error
-        helperText="Incorrecly specified email"
-        fullWidth
-      />
-      <TextField className={styles.field} label="Password" fullWidth />
-      <Button size="large" variant="contained" fullWidth>
-        Log in
-      </Button>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <TextField
+          className={styles.field}
+          label="E-Mail"
+          error={Boolean(errors.email?.message)}
+          helperText={errors.email?.message}
+          type="email"
+          {...register("email", { required: "Specify email" })}
+          fullWidth
+        />
+        <TextField 
+          className={styles.field} 
+          label="Password"
+          error={Boolean(errors.password?.message)}
+          helperText={errors.password?.message}
+          type="password"
+          {...register("password", { required: "Specify password" })} 
+          fullWidth 
+        />
+        <Button size="large" type="submit" variant="contained" fullWidth>
+          Log in
+        </Button>
+      </form>
     </Paper>
   );
 };
